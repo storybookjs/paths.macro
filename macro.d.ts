dbb8ad0efcd2dfeedf7b398e136ad02ec7a22584
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2020-08-16 17:53:10 +0900

    feat: fix type

--- macro.d.ts
+++ macro.d.ts
@@ -2,46 +2,46 @@
  * `/Users/you/project/src/input.js`
  * => `/Users/you/project`
  */
-export const npmRoot: () => string;
+export const npmRoot: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `/Users/you/project`
  */
-export const gitRoot: () => string;
+export const gitRoot: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `/Users/you/project`
  */
-export const wd: () => string;
+export const wd: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `/Users/you/project/src/input.js`
  */
-export const fileAbsolute: () => string;
+export const fileAbsolute: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `input.js`
  */
-export const file: () => string;
+export const file: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `.js`
  */
-export const extension: () => string;
+export const extension: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `input`
  */
-export const filename: () => string;
+export const filename: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `/Users/you/project/src/`
  */
-export const baseAbsolute: () => string;
+export const baseAbsolute: string;
 /**
  * `/Users/you/project/src/input.js`
  * => `/src/`
  */
-export const base: () => string;
+export const base: string;
 
 export default base
